9ac5e3b8d77058082e0f47abe7385b6fd748e66d
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-01-19 17:16:37 +0300

    Update post method

--- main.py
+++ main.py
@@ -3,24 +3,17 @@ import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
 
-#class Item(BaseModel):
-    text: str
-
 app = FastAPI()
-#classifier = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
     return {"message": "Hello UrFU"}
 
-@app.post("/predict/")
-#def predict(item: Item):
-    #return classifier(item.text)[0]
-    
 tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment')
 model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment', return_dict=True)
 
 @torch.no_grad()
+
 def predict(text):
     inputs = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**inputs)
@@ -37,3 +30,8 @@ def predict(text):
 text = input("Введите текст: ")
 
 predict(text)
+
+@app.post("/predict/")
+
+def predict(item: text):
+    return predicted(item.text)[0]
